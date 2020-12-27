7b054accab5cb65571468ffa7f2c755ec65aaf4b
Colin Newell <colin.newell@gmail.com>
2020-12-27 11:57:32 +0000

    Tweak comments

--- internal/mysql/packet/split.go
+++ internal/mysql/packet/split.go
@@ -5,15 +5,13 @@ import "encoding/binary"
 // SplitPacket takes a blob of data and divides it up into MySQL packets.  This
 // allows for data captured to be sent to regular parsing routines in a way
 // that allows them to just consider a packet at a time.  Note that it doesn't
-// really do any serious validation.
+// really do any validation.
 // Returns a list of packets, plus whatever data appears to remain.
 func SplitPacket(data []byte) ([][]byte, []byte) {
 	var packets [][]byte
 
 	var lengthBuffer [4]byte
 	copy(lengthBuffer[:], data[:3])
-	// NOTE: mysql docs say int, and don't appear to mention sign at all, so
-	// this is currently just an assumption.
 	length := binary.LittleEndian.Uint32(lengthBuffer[:])
 
 	for length > 0 {
