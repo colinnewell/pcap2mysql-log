531a7dc110bff3bf3535eb6ac2ca03c11d70343a
Colin Newell <colin.newell@gmail.com>
2020-12-28 10:17:12 +0000

    Start to decode some of the info

--- cmd/pcap2mysql-log/main.go
+++ cmd/pcap2mysql-log/main.go
@@ -39,7 +39,14 @@ func main() {
 		log.Fatal("Must specify --to and --from files with traffic")
 	}
 
-	f, err := os.Open(to)
+	fmt.Println("---- To")
+	dumpFile(to)
+	fmt.Println("---- From")
+	dumpFile(from)
+}
+
+func dumpFile(filename string) {
+	f, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -52,14 +59,33 @@ type mySQLinterpretter struct {
 }
 
 func (m *mySQLinterpretter) Write(p []byte) (int, error) {
-	switch protocol.GetPacketType(p) {
+	switch t := protocol.GetPacketType(p); t {
 	case protocol.ComStmtPrepare:
+		fmt.Println("Prepare")
 	case protocol.ComQuery:
 		decoded, err := protocol.DecodeQueryRequest(p)
-		fmt.Printf("%#v: %v\n", decoded, err)
-		fmt.Println(decoded.Query)
+		if err != nil {
+			fmt.Printf("%v: %#v\n", err, p)
+		} else {
+			fmt.Printf("%#v\n", decoded)
+		}
 	case protocol.ComQuit:
 		fmt.Println("quit")
+	case protocol.ResponseErr:
+		decoded, err := protocol.DecodeErrResponse(p)
+		fmt.Printf("%#v: %v\n", decoded, err)
+	case protocol.ResponseOk:
+		decoded, err := protocol.DecodeOkResponse(p)
+		fmt.Printf("%#v: %v\n", decoded, err)
+	case 0x04:
+		fmt.Println("Field list")
+		fmt.Printf("%#v\n", p)
+		// should expect a bunch of fields followed by an EOF
+		// specifies number of fields to expect
+	case 0xfe:
+		fmt.Println("EOF")
+	default:
+		fmt.Printf("Unrecognised packet: %x\n", t)
 	}
 	return len(p), nil
 }
