79d968759018143e3aa56feab7325e189f7f63e9
Nadja Summerscales-Gunn <n.summerscales-gunn@resume-library.com>
2021-01-03 17:14:53 +0000

    (feat) Update response.go to restructure the switch statement to check for state and switch the state depending on which part of the process it's in. Update the Makefile to look at the internal/mysql folder for changes

--- internal/mysql/decoding/response.go
+++ internal/mysql/decoding/response.go
@@ -4,8 +4,6 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-
-	"github.com/orderbynull/lottip/protocol"
 )
 
 type mySQLtypes struct {
@@ -30,68 +28,68 @@ type mySQLfieldinfo struct {
 
 //MySQLresponse - dealing with the response
 type MySQLresponse struct {
-	Fields     []mySQLtypes
-	InResponse bool
-	EOFCount   int
+	Fields []mySQLtypes
+	State  readState
 }
 
+type readState byte
+
+const (
+	start readState = iota
+	fieldInfo
+	data
+)
+
 func (m *MySQLresponse) Write(p []byte) (int, error) {
-	if m.InResponse {
-		switch t := protocol.GetPacketType(p); t {
-		case protocol.ResponseErr:
-			decoded, err := protocol.DecodeErrResponse(p)
-			fmt.Printf("%#v: %v\n", decoded, err)
-		case protocol.ResponseOk:
-			decoded, err := protocol.DecodeOkResponse(p)
-			fmt.Printf("%#v: %v\n", decoded, err)
-		case 1:
-			//assuming this is the data
-			fmt.Println("Response Data")
-			fmt.Printf("%#v\n", p)
-		case 3:
-			fmt.Println("Field definition")
-			buf := bytes.NewBuffer(p[4:])
-			field := mySQLtypes{}
+	switch m.State {
+	case start:
+		fmt.Printf("Expecting: %d fields\n", p[4])
+		m.State = fieldInfo
+	case data:
+		if p[4] == 0xfe {
+			m.State = start
+			break
+		}
 
-			for _, val := range []*string{
-				&field.Catalog,
-				&field.Schema,
-				&field.TableAlias,
-				&field.Table,
-				&field.ColumnAlias,
-				&field.Column,
-			} {
-				s, err := readString(buf)
+		fmt.Println("Response Data")
+		fmt.Printf("%#v\n", p)
+	case fieldInfo:
+		if p[4] == 0xfe {
+			m.State = data
+			break
+		}
 
-				if err != nil {
-					return 0, err
-				}
+		fmt.Println("Field definition")
 
-				*val = s
-			}
+		buf := bytes.NewBuffer(p[4:])
+		field := mySQLtypes{}
 
-			if err := binary.Read(buf, binary.LittleEndian, &field.FieldInfo); err != nil {
+		for _, val := range []*string{
+			&field.Catalog,
+			&field.Schema,
+			&field.TableAlias,
+			&field.Table,
+			&field.ColumnAlias,
+			&field.Column,
+		} {
+			s, err := readString(buf)
+
+			if err != nil {
 				return 0, err
 			}
 
-			m.Fields = append(m.Fields, field)
-
-			fmt.Printf("%#v\n", field)
-
-		case 0xfe:
-			fmt.Println("EOF")
-			m.EOFCount = m.EOFCount + 1
-
-			if m.EOFCount >= 2 {
-				m.InResponse = false
-				m.EOFCount = 0
-			}
-		default:
-			fmt.Printf("Unrecognised packet: %x\n", t)
+			*val = s
 		}
-	} else {
-		fmt.Printf("Expecting: %d fields\n", p[4])
-		m.InResponse = true
+
+		if err := binary.Read(buf, binary.LittleEndian, &field.FieldInfo); err != nil {
+			return 0, err
+		}
+
+		m.Fields = append(m.Fields, field)
+
+		fmt.Printf("%#v\n", field)
+	default:
+		fmt.Printf("Unrecognised packet: %x\n", p[0])
 	}
 
 	return len(p), nil
