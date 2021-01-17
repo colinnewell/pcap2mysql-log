d19e5f985fa3534469361615dc00db3fea8b1c3d
Colin Newell <c.newell@cv-library.co.uk>
2021-01-17 22:34:47 +0000

    Fix boundary error with copy function

--- internal/mysql/packet/copy_test.go
+++ internal/mysql/packet/copy_test.go
@@ -23,4 +23,26 @@ func TestCopy(t *testing.T) {
 	if diff := cmp.Diff(s.packets, expected); diff != "" {
 		t.Fatalf("Writes don't match (-got +expected):\n%s\n", diff)
 	}
+	if diff := cmp.Diff(err.Error(), "packet: incomplete packet"); diff != "" {
+		t.Fatalf("Errors don't match (-got +expected):\n%s\n", diff)
+	}
+}
+
+func TestCopyNoError(t *testing.T) {
+	b := bytes.NewBuffer([]byte{3, 0, 0, 0, 0x32, 0x33, 0x31, 4, 0, 0, 1, 0x20, 0x20, 0x20, 0x20})
+
+	s := splitter{}
+	n, err := packet.Copy(b, &s)
+	t.Log(n)
+	t.Log(err)
+	expected := [][]byte{
+		{3, 0, 0, 0, 0x32, 0x33, 0x31},
+		{4, 0, 0, 1, 0x20, 0x20, 0x20, 0x20},
+	}
+	if diff := cmp.Diff(s.packets, expected); diff != "" {
+		t.Fatalf("Writes don't match (-got +expected):\n%s\n", diff)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
 }
