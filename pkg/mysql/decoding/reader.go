c13ac3929c994eeaecc8f28ad39421defbfb1968
Colin Newell <colin.newell@gmail.com>
2021-02-28 23:33:23 +0000

    Fix another shady use of a bytes.Buffer

--- pkg/mysql/decoding/reader.go
+++ pkg/mysql/decoding/reader.go
@@ -192,6 +192,13 @@ func (e *RawDataEmitter) Transmission(t interface{}) {
 	// take the buffer that has been emitted and tag that onto what we're
 	// emitting
 	// this feels flawed, should I be copying the byte array?
-	e.emitter.Transmission(structure.WithRawPacket{RawData: e.read.Bytes(), Transmission: t})
+	data := make([]byte, e.read.Len())
+	if e.read.Len() > 0 {
+		if _, err := e.read.Read(data); err != nil {
+			// shouldn't really be possible to have an error here.
+			panic(err)
+		}
+	}
+	e.emitter.Transmission(structure.WithRawPacket{RawData: data, Transmission: t})
 	e.read.Reset()
 }
