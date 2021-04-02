76d9854d6a3ea745e8ed6a793857fbb3a46ea53d
Colin Newell <colin.newell@gmail.com>
2021-04-02 19:03:01 +0100

    Simple test to get an error

--- test/simple_test.go
+++ test/simple_test.go
@@ -11,10 +11,16 @@ import (
 
 func TestSimpleConnection(t *testing.T) {
 	//nolint:lll
-	db, err := sql.Open("mysql", "site:84aaa213dbb7aa3d67d57ba49acc2a71b7c4cd8bf689bfdf4372e4a34dceeca0@tcp(127.0.0.1:3306)/demo")
+	db, err := sql.Open("mysql", "site:84aaa213dbb7aa3d67d57ba49acc2a71b7c4cd8bf689bfdf4372e4a34dceeca0@tcp(localhost:3306)/demo")
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	defer db.Close()
+
+	insert, err := db.Query("INSERT INTO test VALUES ( 2, 'TEST' )")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer insert.Close()
 }
