016212332e45f790ff370c5eb5c9aa487bdb0d7e
Colin Newell <colin.newell@gmail.com>
2021-04-03 14:46:51 +0100

    Add a new test and optimise the docker build

--- test/simple_test.go
+++ test/simple_test.go
@@ -9,16 +9,33 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func TestSimpleConnection(t *testing.T) {
+func connection(t *testing.T) *sql.DB {
+	t.Helper()
+
 	//nolint:lll
 	db, err := sql.Open("mysql", "site:84aaa213dbb7aa3d67d57ba49acc2a71b7c4cd8bf689bfdf4372e4a34dceeca0@tcp(localhost:3306)/demo")
 	if err != nil {
 		t.Fatal(err)
 	}
+	return db
+}
 
+func TestSimpleError(t *testing.T) {
+	db := connection(t)
 	defer db.Close()
 
 	insert, err := db.Query("INSERT INTO test VALUES ( 2, 'TEST' )")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer insert.Close()
+}
+
+func TestSimpleInsert(t *testing.T) {
+	db := connection(t)
+	defer db.Close()
+
+	insert, err := db.Query("INSERT INTO peeps (name, age) VALUES ( ?, ? )", "person", 33)
 	if err != nil {
 		t.Fatal(err)
 	}
