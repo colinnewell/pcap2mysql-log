84e2adb9a0685b1e9262ac3c40468ebfe137716b
Colin Newell <colin.newell@gmail.com>
2021-04-04 15:20:41 +0100

    Normalise timezone to ensure tests work consistently

--- e2e-test.sh
+++ e2e-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-./pcap2mysql-log test/captures/insecure.pcap > test/captures/insecure.actual
+TZ= ./pcap2mysql-log test/captures/insecure.pcap > test/captures/insecure.actual
 diff test/captures/insecure.expected test/captures/insecure.actual
