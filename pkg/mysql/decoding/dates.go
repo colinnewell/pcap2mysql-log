69325d082cda071c02ec8de8dc107cc8490e4aaf
Colin Newell <c.newell@cv-library.co.uk>
2021-05-09 19:06:35 +0100

    Minor rearrangement

---
+++ pkg/mysql/decoding/dates.go
@@ -0,0 +1,166 @@
+package decoding
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/pkg/errors"
+)
+
+// FIXME: does date have time set to 0 or actually just take this much?
+type date struct {
+	Length uint8
+	Year   uint16
+	Month  uint8
+	Day    uint8
+}
+
+func (d date) String() string {
+	return fmt.Sprintf("%04d-%02d-%02d",
+		d.Year,
+		d.Month,
+		d.Day,
+	)
+}
+
+func (d date) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.String())
+}
+
+type timeS struct {
+	Hour    uint8
+	Minutes uint8
+	Seconds uint8
+}
+
+func (t timeS) String() string {
+	return fmt.Sprintf(
+		"%d:%d:%d",
+		t.Hour,
+		t.Minutes,
+		t.Seconds,
+	)
+}
+
+type timeMs struct {
+	timeS
+	MicroSeconds uint32
+}
+
+type dateTime struct {
+	date
+	timeS
+}
+
+type dateTimeMs struct {
+	date
+	timeMs
+}
+
+func (d dateTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(fmt.Sprintf(
+		"%s %s",
+		d.date.String(),
+		d.timeS.String(),
+	))
+}
+
+func (d dateTimeMs) MarshalJSON() ([]byte, error) {
+	return json.Marshal(fmt.Sprintf(
+		"%s %s.%04d",
+		d.date.String(),
+		d.timeS.String(),
+		d.MicroSeconds,
+	))
+}
+
+type timeInfo struct {
+	Length   uint8
+	Negative uint8
+	Date     uint32
+	Hour     uint8
+	Minutes  uint8
+	Seconds  uint8
+}
+
+type timeInfoMs struct {
+	timeInfo
+	MicroSeconds uint32
+}
+
+func readDate(buf io.Reader) (interface{}, error) {
+	var d date
+	if err := binary.Read(buf, binary.LittleEndian, &d); err != nil {
+		return nil, errors.Wrap(err, "decode-execute")
+	}
+
+	//nolint:gomnd
+	if d.Length == 4 {
+		return d, nil
+	}
+	var t timeS
+	if err := binary.Read(buf, binary.LittleEndian, &t); err != nil {
+		return nil, errors.Wrap(err, "decode-execute")
+	}
+	//nolint:gomnd
+	if d.Length == 7 {
+		return dateTime{
+			date:  d,
+			timeS: t,
+		}, nil
+	}
+	var ms uint32
+	if err := binary.Read(buf, binary.LittleEndian, &ms); err != nil {
+		return nil, errors.Wrap(err, "decode-execute")
+	}
+
+	return dateTimeMs{
+		date: d,
+		timeMs: timeMs{
+			timeS:        t,
+			MicroSeconds: ms,
+		},
+	}, nil
+	// FIXME: would be good to know locale for timestamp from server info assuming that's provided.
+	// byte position	description
+	// 1	data length : 4 without hour/minute/second part, 7 without fractional seconds, 11 with fractional seconds
+	// 2-3	year on 2 bytes little-endian format
+	// 4	Month ( 1=january)
+	// 5	days of month
+
+	// 6	hour of day (0 if DATE type)
+	// 7	minutes (0 if DATE type)
+	// 8	secondes (0 if DATE type)
+
+	// 9-12	micro-second on 4 bytes little-endian format (only if data-length is > 7)
+}
+
+func readTime(buf io.Reader) (interface{}, error) {
+	var t timeInfo
+	if err := binary.Read(buf, binary.LittleEndian, &t); err != nil {
+		return nil, errors.Wrap(err, "decode-execute")
+	}
+
+	//nolint:gomnd
+	if t.Length == 12 {
+		// read microseconds
+		var ms uint32
+		if err := binary.Read(buf, binary.LittleEndian, &ms); err != nil {
+			return nil, errors.Wrap(err, "decode-execute")
+		}
+		return timeInfoMs{timeInfo: t, MicroSeconds: ms}, nil
+	}
+
+	return t, nil
+	// byte position	description
+	// 1	data length : 8 without fractional seconds, 12 with fractional seconds
+	// 2	is negative
+	// 3-6	date on 4 bytes little-endian format
+	// 7	hour of day
+	// 8	minutes
+	// 9	secondes
+
+	// 10-13	micro-second on 4 bytes little-endian format (only if data-length is > 7)
+}
--- pkg/mysql/decoding/packet.go
+++ pkg/mysql/decoding/packet.go
@@ -3,7 +3,6 @@ package decoding
 import (
 	"bytes"
 	"encoding/binary"
-	"encoding/json"
 	"fmt"
 	"io"
 	"unicode"
@@ -14,87 +13,6 @@ import (
 
 var errRequestTooManyBytes = errors.New("requesting more bytes than are in the packet")
 var errRequestTooFewBytes = errors.New("not enough bytes")
-
-// FIXME: does date have time set to 0 or actually just take this much?
-type date struct {
-	Length uint8
-	Year   uint16
-	Month  uint8
-	Day    uint8
-}
-
-func (d date) String() string {
-	return fmt.Sprintf("%04d-%02d-%02d",
-		d.Year,
-		d.Month,
-		d.Day,
-	)
-}
-
-func (d date) MarshalJSON() ([]byte, error) {
-	return json.Marshal(d.String())
-}
-
-type timeS struct {
-	Hour    uint8
-	Minutes uint8
-	Seconds uint8
-}
-
-func (t timeS) String() string {
-	return fmt.Sprintf(
-		"%d:%d:%d",
-		t.Hour,
-		t.Minutes,
-		t.Seconds,
-	)
-}
-
-type timeMs struct {
-	timeS
-	MicroSeconds uint32
-}
-
-type dateTime struct {
-	date
-	timeS
-}
-
-type dateTimeMs struct {
-	date
-	timeMs
-}
-
-func (d dateTime) MarshalJSON() ([]byte, error) {
-	return json.Marshal(fmt.Sprintf(
-		"%s %s",
-		d.date.String(),
-		d.timeS.String(),
-	))
-}
-
-func (d dateTimeMs) MarshalJSON() ([]byte, error) {
-	return json.Marshal(fmt.Sprintf(
-		"%s %s.%04d",
-		d.date.String(),
-		d.timeS.String(),
-		d.MicroSeconds,
-	))
-}
-
-type timeInfo struct {
-	Length   uint8
-	Negative uint8
-	Date     uint32
-	Hour     uint8
-	Minutes  uint8
-	Seconds  uint8
-}
-
-type timeInfoMs struct {
-	timeInfo
-	MicroSeconds uint32
-}
 
 func readLenEncString(buf *bytes.Buffer) (string, error) {
 	count, err := buf.ReadByte()
@@ -187,7 +105,6 @@ func readLenEncInt(buf io.Reader) (uint64, error) {
 // 	return string(b), nil
 // }
 
-//nolint:gocognit
 func readType(buf *bytes.Buffer, fieldType structure.FieldType) (interface{}, error) {
 	switch fieldType {
 	case structure.FLOAT:
@@ -252,77 +169,10 @@ func readType(buf *bytes.Buffer, fieldType structure.FieldType) (interface{}, er
 	case structure.DATE,
 		structure.DATETIME,
 		structure.TIMESTAMP:
-		var d date
-		if err := binary.Read(buf, binary.LittleEndian, &d); err != nil {
-			return nil, errors.Wrap(err, "decode-execute")
-		}
-
-		//nolint:gomnd
-		if d.Length == 4 {
-			return d, nil
-		}
-		var t timeS
-		if err := binary.Read(buf, binary.LittleEndian, &t); err != nil {
-			return nil, errors.Wrap(err, "decode-execute")
-		}
-		//nolint:gomnd
-		if d.Length == 7 {
-			return dateTime{
-				date:  d,
-				timeS: t,
-			}, nil
-		}
-		var ms uint32
-		if err := binary.Read(buf, binary.LittleEndian, &ms); err != nil {
-			return nil, errors.Wrap(err, "decode-execute")
-		}
-
-		return dateTimeMs{
-			date: d,
-			timeMs: timeMs{
-				timeS:        t,
-				MicroSeconds: ms,
-			},
-		}, nil
-		// FIXME: would be good to know locale for timestamp from server info assuming that's provided.
-		// byte position	description
-		// 1	data length : 4 without hour/minute/second part, 7 without fractional seconds, 11 with fractional seconds
-		// 2-3	year on 2 bytes little-endian format
-		// 4	Month ( 1=january)
-		// 5	days of month
-
-		// 6	hour of day (0 if DATE type)
-		// 7	minutes (0 if DATE type)
-		// 8	secondes (0 if DATE type)
-
-		// 9-12	micro-second on 4 bytes little-endian format (only if data-length is > 7)
+		return readDate(buf)
 
 	case structure.TIME:
-		var t timeInfo
-		if err := binary.Read(buf, binary.LittleEndian, &t); err != nil {
-			return nil, errors.Wrap(err, "decode-execute")
-		}
-
-		//nolint:gomnd
-		if t.Length == 12 {
-			// read microseconds
-			var ms uint32
-			if err := binary.Read(buf, binary.LittleEndian, &ms); err != nil {
-				return nil, errors.Wrap(err, "decode-execute")
-			}
-			return timeInfoMs{timeInfo: t, MicroSeconds: ms}, nil
-		}
-
-		return t, nil
-		// byte position	description
-		// 1	data length : 8 without fractional seconds, 12 with fractional seconds
-		// 2	is negative
-		// 3-6	date on 4 bytes little-endian format
-		// 7	hour of day
-		// 8	minutes
-		// 9	secondes
-
-		// 10-13	micro-second on 4 bytes little-endian format (only if data-length is > 7)
+		return readTime(buf)
 
 	case structure.STRING,
 		structure.VAR_STRING,
