b1e01070df4568e7fbf17d84d1c28a8bbbdd4aaf
Colin Newell <colin.newell@gmail.com>
2021-05-30 13:38:38 +0100

    Rename to appease the linter

--- pkg/mysql/packet/packet_buffer.go
+++ pkg/mysql/packet/packet_buffer.go
@@ -15,15 +15,15 @@ type Packet struct {
 	Data []byte
 }
 
-// PacketBuffer buffers MySQL packets along with their times seen so they can
+// Buffer buffers MySQL packets along with their times seen so they can
 // be played back in order.
-type PacketBuffer struct {
+type Buffer struct {
 	Times   TimesSeen
 	packets []Packet
 }
 
 // Write buffers up the packets and stores when they were seen.
-func (b *PacketBuffer) Write(p []byte) (n int, err error) {
+func (b *Buffer) Write(p []byte) (n int, err error) {
 	packet := Packet{Data: p, Seen: b.Times.Seen()}
 	b.Times.Reset()
 	if len(packet.Seen) == 0 {
