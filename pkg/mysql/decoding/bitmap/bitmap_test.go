4014a70868dcaf8c74fb02f48a20bc25e57e476e
Colin Newell <colin.newell@gmail.com>
2021-05-31 13:08:33 +0100

    Fix bitmap misunderstanding

--- pkg/mysql/decoding/bitmap/bitmap_test.go
+++ pkg/mysql/decoding/bitmap/bitmap_test.go
@@ -20,3 +20,14 @@ func TestParamBM(t *testing.T) {
 		t.Error("param 2 should be null")
 	}
 }
+
+func TestCrash(t *testing.T) {
+	bytes := bytes.NewBuffer([]byte{0, 1, 0, 128})
+	nm, err := bitmap.ReadNullMap(bytes, 30, bitmap.ResultSetRow)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if nm.IsNull(24) {
+		t.Error("param 24 should not be null")
+	}
+}
