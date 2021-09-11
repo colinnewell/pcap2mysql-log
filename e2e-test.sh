0f50084b9f807517305ea9b7b93a73056f082591
Colin Newell <colin.newell@gmail.com>
2021-09-11 17:39:00 +0100

    Exercise the raw data flag

--- e2e-test.sh
+++ e2e-test.sh
@@ -11,3 +11,12 @@ do
     fi
     diff -q $FILE.expected $FILE.actual || (echo Failed diff $FILE.expected $FILE.actual && exit 1)
 done
+
+
+FILE=test/captures/compressed.raw
+TZ= ./pcap2mysql-log test/captures/compressed.pcap --raw-data > $FILE.actual
+if [ ! -f $FILE.expected ]
+then
+    cp $FILE.actual $FILE.expected
+fi
+diff -q $FILE.expected $FILE.actual || (echo Failed diff $FILE.expected $FILE.actual && exit 1)
