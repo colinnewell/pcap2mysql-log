538f2774ea5e7e4bc80d7c35131929888a1653b6
Colin Newell <c.newell@cv-library.co.uk>
2022-01-02 19:49:10 +0000

    Note about the message format

--- ensure-gofmt.sh
+++ ensure-gofmt.sh
@@ -6,6 +6,8 @@ then
     echo "Files need to be gofmt'd."
     for f in $list
     do
+        # this format means if an editor
+        # is reading the output it can take you to the file.
         echo $f:1:0: Need to run gofmt -w -s $f
     done
     exit 1
